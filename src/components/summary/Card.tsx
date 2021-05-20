4ae6bf0c11272b281090d8c1576b78c6f38418a3
Adithya NR <adithyabhat17.ab@gmail.com>
2021-05-21 00:02:27 +0530

    Update Card.tsx

--- src/components/summary/Card.tsx
+++ src/components/summary/Card.tsx
@@ -1,80 +1,30 @@
-import { Flex, Grid, Heading } from "@chakra-ui/layout";
-import { Collapse } from "@chakra-ui/transition";
-import { memo, useMemo } from "react";
-import { useCurrency } from "../../context/exchange";
-import { SUPPORTED_CURRENCIES } from "../../lib/exchangeRates";
-import { CryptoListLatest } from "../../types/api";
-import SummaryCard from "./Card";
+import { memo } from "react";
+import { Box, Heading, Text, Stack } from "@chakra-ui/react";
 
 interface Props {
-  holdings: {
-    [key: string]: string;
-  };
-  currencies: CryptoListLatest["data"];
+  title: string;
+  value: string | number;
 }
 
-function Summary({ holdings, currencies }: Props) {
-  const { currency } = useCurrency();
-
-  const totalPortfolioValue = useMemo(() => {
-    return currencies
-      .reduce((total, current) => {
-        return (
-          total +
-          current.quote.USD.price *
-            (Number(holdings[current.id]) || 0) *
-            SUPPORTED_CURRENCIES[currency].rate
-        );
-      }, 0)
-      .toLocaleString();
-  }, [currencies, currency, holdings]);
-
-  const marketCap = useMemo(() => {
-    return (
-      (
-        currencies.reduce((total, current) => {
-          return (
-            total +
-            current.quote.USD.price *
-              SUPPORTED_CURRENCIES[currency].rate *
-              current.circulating_supply
-          );
-        }, 0) / 1e12
-      ) // Divide the total market cap by 1 Trillion
-        .toFixed(2) + "T"
-    );
-  }, [currencies, currency]);
-
+function SummaryCard({ title, value }: Props) {
   return (
-    <Collapse in={true} animateOpacity>
-      <Flex wrap="wrap" justifyContent="space-between" alignItems="center">
-        <Heading size="lg" my={{ base: "5", lg: "0" }}>
-          Top 100 Cryptocurrencies
+    <Box
+      pl="5"
+      w={{ base: "100%", md: "auto" }}
+      pr="10"
+      py="5"
+      border="1px solid"
+      borderColor="grey"
+      borderRadius="xl"
+    >
+      <Stack>
+        <Text isTruncated>{title}</Text>
+        <Heading isTruncated color="purple" size="md">
+          {value}
         </Heading>
-        <Grid
-          overflowX="auto"
-          gridGap="5"
-          width={{ base: "100%", md: "auto" }}
-          gridTemplateColumns="repeat(3, 1fr)"
-        >
-          <SummaryCard
-            title="Portfolio Value"
-            value={SUPPORTED_CURRENCIES[currency].symbol + totalPortfolioValue}
-          />
-          <SummaryCard
-            title="Cryptos Invested"
-            value={
-              Object.keys(holdings).filter((h) => holdings[h] !== "0").length
-            }
-          />
-          <SummaryCard
-            title="Market Cap"
-            value={SUPPORTED_CURRENCIES[currency].symbol + marketCap}
-          />
-        </Grid>
-      </Flex>
-    </Collapse>
+      </Stack>
+    </Box>
   );
 }
 
-export default memo(Summary);
+export default memo(SummaryCard);
